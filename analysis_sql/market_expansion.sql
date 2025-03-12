4982ee95d0c96d4a8613db6a41e27af0c8c8989c
Lê Quang Giảng <lequanggiang.hrm@gmail.com>
2025-03-12 10:41:22 +0700

    Add files via upload

---
+++ analysis_sql/market_expansion.sql
@@ -0,0 +1,81 @@
+﻿/*
+ANALYSIS & FINDING INSIGHTS
+*/
+
+-- IV. Phân tích Khu vực Bán hàng & Mở rộng Thị trường
+
+-- Khu vực nào mang lại doanh thu, lợi nhuận, biên lợi nhuận cao nhất?
+WITH SalesData AS (
+    SELECT 
+		fg AS KenhPhanPhoi,
+		[Nhóm bán hàng] AS MaVungBanHang,
+        [Tên Vùng bán hàng] AS VungBanHang,
+        SUM([Doanh thu]) AS TongDoanhThu,
+        SUM([Doanh thu] - [Giá vốn] - [Chi phí]) AS LoiNhuanRong,
+        CASE 
+            WHEN SUM([Doanh thu]) = 0 THEN 0 
+            ELSE ROUND((SUM([Doanh thu] - [Giá vốn] - [Chi phí]) / SUM([Doanh thu])) * 100, 2) 
+        END AS BienLoiNhuan
+    FROM OfficeSupplies.dbo.Sales_Analysis_OfficeSupplies
+    GROUP BY [Tên Vùng bán hàng], [Nhóm bán hàng], fg
+)
+
+SELECT 
+	KenhPhanPhoi,
+	MaVungBanHang,
+    VungBanHang,
+    TongDoanhThu,
+    LoiNhuanRong,
+    BienLoiNhuan,
+    RANK() OVER (ORDER BY TongDoanhThu DESC) AS XepHangDoanhThu,
+    RANK() OVER (ORDER BY LoiNhuanRong DESC) AS XepHangLoiNhuan,
+    RANK() OVER (ORDER BY BienLoiNhuan DESC) AS XepHangBienLoiNhuan
+FROM SalesData;
+
+-- Xu hướng tăng trưởng doanh số của từng khu vực theo thời gian như thế nào?
+WITH SalesByRegion AS (
+    -- Tính tổng doanh thu theo từng khu vực và từng quý
+    SELECT 
+        [Tên Vùng bán hàng], 
+        CONCAT(YEAR([Kỳ]), ' Q', DATEPART(QUARTER, [Kỳ])) AS [Quý], 
+        SUM([Doanh thu]) AS [Tổng Doanh Thu]
+    FROM OfficeSupplies.dbo.Sales_Analysis_OfficeSupplies
+    GROUP BY [Tên Vùng bán hàng], YEAR([Kỳ]), DATEPART(QUARTER, [Kỳ])
+),
+RankedSales AS (
+    -- Gán số thứ tự theo thời gian cho từng vùng bán hàng
+    SELECT 
+        [Tên Vùng bán hàng], 
+        [Quý], 
+        [Tổng Doanh Thu],
+        ROW_NUMBER() OVER (PARTITION BY [Tên Vùng bán hàng] ORDER BY [Quý]) AS rn_asc,
+        ROW_NUMBER() OVER (PARTITION BY [Tên Vùng bán hàng] ORDER BY [Quý] DESC) AS rn_desc
+    FROM SalesByRegion
+),
+FirstLastSales AS (
+    -- Lấy doanh thu của quý đầu tiên và quý mới nhất
+    SELECT 
+        f.[Tên Vùng bán hàng],
+        f.[Tổng Doanh Thu] AS [Doanh Thu Quý Đầu],
+        l.[Tổng Doanh Thu] AS [Doanh Thu Quý Mới Nhất]
+    FROM RankedSales f
+    JOIN RankedSales l ON f.[Tên Vùng bán hàng] = l.[Tên Vùng bán hàng]
+    WHERE f.rn_asc = 1 AND l.rn_desc = 1
+)
+SELECT 
+    [Tên Vùng bán hàng], 
+    [Doanh Thu Quý Đầu], 
+    [Doanh Thu Quý Mới Nhất], 
+    ([Doanh Thu Quý Mới Nhất] - [Doanh Thu Quý Đầu]) AS [Tăng Trưởng Tuyệt Đối],
+    CASE 
+        WHEN [Doanh Thu Quý Đầu] > 0 THEN 
+            ([Doanh Thu Quý Mới Nhất] - [Doanh Thu Quý Đầu]) * 100.0 / [Doanh Thu Quý Đầu] 
+        ELSE NULL 
+    END AS [Tăng Trưởng Tỷ Lệ (%)],
+    CASE 
+        WHEN [Doanh Thu Quý Mới Nhất] > [Doanh Thu Quý Đầu] THEN 'Tăng'
+        WHEN [Doanh Thu Quý Mới Nhất] < [Doanh Thu Quý Đầu] THEN 'Giảm'
+        ELSE 'Không đổi'
+    END AS [Xu Hướng]
+FROM FirstLastSales
+ORDER BY [Tăng Trưởng Tỷ Lệ (%)] DESC;
---
+++ analysis_sql/product_development_orientation.sql
@@ -0,0 +1,194 @@
+﻿/*
+ANALYSIS & FINDING INSIGHTS
+*/
+
+-- II. Định hướng Phát triển Sản phẩm
+
+-- Sản phẩm nào có doanh thu cao nhất nhưng lợi nhuận ròng thấp? 
+WITH AvgMargin AS (
+    SELECT 
+        AVG(SUM([Doanh thu] - [Giá vốn] - [Chi phí]) * 100.0 / NULLIF(SUM([Doanh thu]), 0)) 
+        OVER () AS Bien_Loi_Nhuan_Trung_Binh
+    FROM OfficeSupplies.dbo.Sales_Analysis_OfficeSupplies
+)
+SELECT TOP 10 
+    s.[Mã sản phẩm], 
+    s.[Tên sản phẩm], 
+    SUM(s.[Doanh thu]) AS Tong_Doanh_Thu, 
+    SUM(s.[Doanh thu] - s.[Giá vốn] - s.[Chi phí]) AS Loi_Nhuan_Rong,
+    (SUM(s.[Doanh thu] - s.[Giá vốn] - s.[Chi phí]) * 100.0 / NULLIF(SUM(s.[Doanh thu]), 0)) AS Bien_Loi_Nhuan,
+    a.Bien_Loi_Nhuan_Trung_Binh
+FROM OfficeSupplies.dbo.Sales_Analysis_OfficeSupplies s
+CROSS JOIN AvgMargin a
+GROUP BY s.[Mã sản phẩm], s.[Tên sản phẩm], a.Bien_Loi_Nhuan_Trung_Binh
+HAVING (SUM(s.[Doanh thu] - s.[Giá vốn] - s.[Chi phí]) * 100.0 / NULLIF(SUM(s.[Doanh thu]), 0)) < a.Bien_Loi_Nhuan_Trung_Binh
+ORDER BY Tong_Doanh_Thu DESC, Loi_Nhuan_Rong ASC;
+
+-- Sản phẩm nào có biên lợi nhuận tốt nhất? 
+SELECT TOP 10 
+    [Mã sản phẩm], 
+    [Tên sản phẩm], 
+    SUM([Doanh thu]) AS Tong_Doanh_Thu, 
+    SUM([Doanh thu] - [Giá vốn] - [Chi phí]) AS Loi_Nhuan_Rong,
+    (SUM([Doanh thu] - [Giá vốn] - [Chi phí]) * 100.0 / NULLIF(SUM([Doanh thu]), 0)) AS Bien_Loi_Nhuan
+FROM OfficeSupplies.dbo.Sales_Analysis_OfficeSupplies
+GROUP BY [Mã sản phẩm], [Tên sản phẩm]
+ORDER BY Bien_Loi_Nhuan DESC;
+
+-- Nhóm sản phẩm nào đang có xu hướng tăng trưởng mạnh nhất?
+	-- Nhóm SP - Cấp 1
+	WITH DoanhThuTheoQuy AS (
+		SELECT 
+			[Tên Nhóm SP - Cấp 1] AS NhomSanPham,
+			DATEFROMPARTS(YEAR([Kỳ]), (DATEPART(QUARTER, [Kỳ]) - 1) * 3 + 1, 1) AS NgayQuy, 
+			SUM([Doanh thu]) AS TongDoanhThu
+		FROM OfficeSupplies.dbo.Sales_Analysis_OfficeSupplies
+		GROUP BY [Tên Nhóm SP - Cấp 1], YEAR([Kỳ]), DATEPART(QUARTER, [Kỳ])
+	),
+	TangTruong AS (
+		SELECT 
+			a.NhomSanPham,
+			a.NgayQuy AS QuyHienTai,
+			b.NgayQuy AS QuyTruoc,
+			a.TongDoanhThu AS DoanhThuHienTai,
+			b.TongDoanhThu AS DoanhThuTruoc,
+			CASE 
+				WHEN b.TongDoanhThu = 0 OR b.TongDoanhThu IS NULL THEN NULL 
+				ELSE (a.TongDoanhThu - b.TongDoanhThu) / b.TongDoanhThu * 100 
+			END AS TyLeTangTruong
+		FROM DoanhThuTheoQuy a
+		LEFT JOIN DoanhThuTheoQuy b 
+			ON a.NhomSanPham = b.NhomSanPham 
+			AND a.NgayQuy = DATEADD(QUARTER, 1, b.NgayQuy)
+	)
+	SELECT
+		NhomSanPham, 
+		AVG(TyLeTangTruong) AS TyLeTangTruongTB
+	FROM TangTruong
+	WHERE TyLeTangTruong IS NOT NULL
+	GROUP BY NhomSanPham
+	ORDER BY TyLeTangTruongTB DESC;
+
+	-- Nhóm SP - Chi tiết
+	WITH DoanhThuTheoQuy AS (
+		SELECT 
+			[Tên Nhóm SP - Cấp 1] AS NhomCap1,
+			[Tên Nhóm SP - Cấp 2] AS NhomCap2,
+			[Tên Nhóm SP - Cấp 3] AS NhomCap3,
+			[Tên Nhóm SP - Cấp 4] AS NhomCap4,
+			DATEFROMPARTS(YEAR([Kỳ]), (DATEPART(QUARTER, [Kỳ]) - 1) * 3 + 1, 1) AS NgayQuy, 
+			SUM([Doanh thu]) AS TongDoanhThu
+		FROM OfficeSupplies.dbo.Sales_Analysis_OfficeSupplies
+		GROUP BY 
+			[Tên Nhóm SP - Cấp 1], 
+			[Tên Nhóm SP - Cấp 2], 
+			[Tên Nhóm SP - Cấp 3], 
+			[Tên Nhóm SP - Cấp 4], 
+			YEAR([Kỳ]), DATEPART(QUARTER, [Kỳ])
+	),
+	TangTruong AS (
+		SELECT 
+			a.NhomCap1,
+			a.NhomCap2,
+			a.NhomCap3,
+			a.NhomCap4,
+			a.NgayQuy AS QuyHienTai,
+			b.NgayQuy AS QuyTruoc,
+			a.TongDoanhThu AS DoanhThuHienTai,
+			b.TongDoanhThu AS DoanhThuTruoc,
+			CASE 
+				WHEN b.TongDoanhThu = 0 OR b.TongDoanhThu IS NULL THEN NULL 
+				ELSE (a.TongDoanhThu - b.TongDoanhThu) / b.TongDoanhThu * 100 
+			END AS TyLeTangTruong
+		FROM DoanhThuTheoQuy a
+		LEFT JOIN DoanhThuTheoQuy b 
+			ON a.NhomCap1 = b.NhomCap1 
+			AND a.NhomCap2 = b.NhomCap2
+			AND a.NhomCap3 = b.NhomCap3
+			AND a.NhomCap4 = b.NhomCap4
+			AND a.NgayQuy = DATEADD(QUARTER, 1, b.NgayQuy)
+	)
+	SELECT TOP 10 
+		NhomCap1 AS [Nhóm SP - Cấp 1],
+		NhomCap2 AS [Nhóm SP - Cấp 2],
+		NhomCap3 AS [Nhóm SP - Cấp 3],
+		NhomCap4 AS [Nhóm SP - Cấp 4],
+		AVG(TyLeTangTruong) AS TyLeTangTruongTB
+	FROM TangTruong
+	WHERE TyLeTangTruong IS NOT NULL
+	GROUP BY NhomCap1, NhomCap2, NhomCap3, NhomCap4
+	ORDER BY TyLeTangTruongTB DESC;
+
+-- Có sản phẩm nào đang suy giảm doanh số qua các kỳ không?
+WITH DoanhThuTheoKy AS (
+    SELECT 
+        [Mã sản phẩm] AS MaSP,
+        [Tên sản phẩm] AS TenSP,
+        [Tên Nhóm SP - Cấp 1] AS NhomSP,
+        DATEFROMPARTS(YEAR([Kỳ]), (DATEPART(QUARTER, [Kỳ]) - 1) * 3 + 1, 1) AS Quy, 
+        SUM([Doanh thu]) AS TongDoanhThu
+    FROM OfficeSupplies.dbo.Sales_Analysis_OfficeSupplies
+    GROUP BY [Mã sản phẩm], [Tên sản phẩm], [Tên Nhóm SP - Cấp 1],
+             DATEFROMPARTS(YEAR([Kỳ]), (DATEPART(QUARTER, [Kỳ]) - 1) * 3 + 1, 1)
+),
+SuyGiam AS (
+    SELECT 
+        a.MaSP,
+        a.TenSP,
+        a.NhomSP,
+        a.Quy AS QuyHienTai,
+        b.Quy AS QuyTruoc,
+        a.TongDoanhThu AS DoanhThuHienTai,
+        b.TongDoanhThu AS DoanhThuTruoc,
+        CASE 
+            WHEN b.TongDoanhThu = 0 OR b.TongDoanhThu IS NULL THEN NULL 
+            ELSE (a.TongDoanhThu - b.TongDoanhThu) / b.TongDoanhThu * 100 
+        END AS TyLeGiam
+    FROM DoanhThuTheoKy a
+    LEFT JOIN DoanhThuTheoKy b 
+        ON a.MaSP = b.MaSP 
+        AND DATEADD(QUARTER, -1, a.Quy) = b.Quy
+    WHERE a.TongDoanhThu < b.TongDoanhThu  
+)
+SELECT TOP 10
+    MaSP AS [Mã sản phẩm],
+    TenSP AS [Tên sản phẩm],
+    NhomSP AS [Tên Nhóm SP - Cấp 1],
+    COUNT(*) AS SoKyGiamLienTuc,
+    AVG(TyLeGiam) AS TyLeGiamTB
+FROM SuyGiam
+WHERE TyLeGiam IS NOT NULL
+GROUP BY MaSP, TenSP, NhomSP
+HAVING COUNT(*) >= 2  
+ORDER BY TyLeGiamTB ASC;
+
+-- Sản phẩm nào có chi phí quá cao so với lợi nhuận mang lại?
+WITH PhanTichChiPhi AS (
+    SELECT 
+        [Mã sản phẩm] AS MaSP,
+        [Tên sản phẩm] AS TenSP,
+        [Tên Nhóm SP - Cấp 1] AS NhomSP,
+        SUM([Doanh thu]) AS TongDoanhThu,
+        SUM([Giá vốn]) AS TongGiaVon,
+        SUM([Chi phí]) AS TongChiPhi,
+        SUM([Doanh thu]) - SUM([Giá vốn]) AS LoiNhuanGop,
+        SUM([Doanh thu]) - SUM([Giá vốn]) - SUM([Chi phí]) AS LoiNhuanRong
+    FROM OfficeSupplies.dbo.Sales_Analysis_OfficeSupplies
+    GROUP BY [Mã sản phẩm], [Tên sản phẩm], [Tên Nhóm SP - Cấp 1]
+)
+SELECT TOP 10
+    MaSP AS [Mã sản phẩm],
+    TenSP AS [Tên sản phẩm],
+    NhomSP AS [Tên Nhóm SP - Cấp 1],
+    TongDoanhThu AS [Tổng doanh thu],
+    TongGiaVon AS [Tổng giá vốn],
+    TongChiPhi AS [Tổng chi phí],
+    LoiNhuanGop AS [Lợi nhuận gộp],
+    LoiNhuanRong AS [Lợi nhuận ròng],
+    CASE 
+        WHEN LoiNhuanRong <= 0 THEN NULL  
+        ELSE (TongChiPhi / LoiNhuanRong) * 100 
+    END AS TyLeChiPhi
+FROM PhanTichChiPhi
+WHERE (LoiNhuanRong <= 0 OR (TongChiPhi / NULLIF(LoiNhuanRong, 0)) > 0.8)
+ORDER BY TyLeChiPhi DESC, LoiNhuanRong ASC;
